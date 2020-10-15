185277e8498a11a8b1e3b881597be133aac7327f
Junhyuck <mrbartrns@naver.com>
2020-10-15 16:48:38 +0900

    removed comment

--- portal/middleware/auth.js
+++ portal/middleware/auth.js
@@ -6,19 +6,6 @@ const {TokenBlackList} = require('../models/token');
 let auth = (req, res, next) => {
     let token = req.cookies.x_auth;
 
-    // User.findByToken(token)
-    // .then(user => {
-    //     if (!user) return res.json({isAuth: false, error: true});
-    //     // request 객체에 token과 user 정보를 넘겨준다.
-    //     req.token = token;
-    //     req.user = user;
-
-    //     // 미들웨어 함수에서 다음으로 진행하기 위해서는 next가 필요
-    //     next();
-    // })
-    // .catch(err => {
-    //     throw err;
-    // });
     TokenBlackList.isExpired(token)
         .then(isExpired => {
             if (isExpired) {
