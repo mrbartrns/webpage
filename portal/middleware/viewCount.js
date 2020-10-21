56391af7e9777bd9329186570d80d3431f817eb2
Junhyuck <mrbartrns@naver.com>
2020-10-21 16:16:42 +0900

    count view with ip

--- portal/middleware/viewCount.js
+++ portal/middleware/viewCount.js
@@ -1,11 +1,46 @@
 const { Post } = require('../models/post');
+const { Ip } = require('../models/ip');
 
-// it is middleware
+const delta = 600000;
+
 // 새로고침시마다 viewCount를 증가시키지 말고 한번 증가 후 얼마 있다가 증가로
-let views = (req, res, next) => {
-    // console.log(req);
-    console.log(req);
-    next();
+let view = (req, res, next) => {
+    const date = Date.now();
+    Ip
+        .findOne({ip: req.ip})
+        .then(ip => {
+            console.log('found ip Document')
+            if (!ip) {
+                console.log('document not found')
+                const ip = new Ip();
+
+                ip.ip = req.ip;
+                ip.expiresIn = date + delta;
+
+                ip
+                    .save()
+                    .then(_ => {
+                        Post
+                            .findOne({_id: req.params.postid})
+                            .then(post => post.updateView())
+                            .then(post => console.log('new post view:', post.views))
+                            .catch(err => console.error(err));
+                        next();
+                    })
+            } else {
+                if (ip.expiresIn < date) {
+                    Post
+                        .findOne({_id: req.params.postid})
+                        .then(post => post.updateView())
+                        .then(_ => Ip.findOneAndUpdate({ip: ip.ip}, {expiresIn: newtime = date + delta}))
+                        .catch(err => console.error(err));
+                }
+                next();
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        })
 };
 
-module.exports = { views };
+module.exports = { view };
