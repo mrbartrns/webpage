08246116fdf0455dfb496a1c5f46910211409f45
Junhyuck <mrbartrns@naver.com>
2020-10-22 16:53:47 +0900

    refactoring

--- portal/routes/login.js
+++ portal/routes/login.js
@@ -17,9 +17,10 @@ module.exports = (app, User) => {
             if (err) console.error;
             user.salt = buf.toString('base64');
             // save returns Promise object (resolve userinfo)
-            user.save()
-                .then(() => res.status(200).json({success: true}))
-                .catch((err) => res.json({success: false, err}));
+            user
+                .save()
+                .then(_ => res.status(200).json({success: true}))
+                .catch(err => res.json({success: false, err}));
         });
     });
 
@@ -30,43 +31,41 @@ module.exports = (app, User) => {
 
     // inspect valid value when login
     app.post('/login', (req, res) => {
-        User.findOne({id: req.body.id}, (err, user) => {
+        User
+            .findOne({id: req.body.id})
+            .then(user => {
+                if (!user) res.json({success: false, message:'존재하지 않는 ID'})
+                user
+                    .comparePassword(req.body.pw)
+                    .then(isMatch => {
+                        if(!isMatch) {
+                            return res.json({
+                                loginSuccess: false,
+                                message: '비밀번호가 일치하지 않습니다.'
+                            });
+                        }
+                        user
+                            .generateToken()
+                            .then(user => {
+                                console.log('got this step')
+                                res
+                                    .cookie("x_auth", user.token)
+                                    .status(200)
+                                    .json({
+                                    loginSuccess: true,
+                                    token: user.token
+                                    });
+                            })
+                    })
+            })
+            .catch(err => {
+                console.error(err);
+                res.json({loginSuccess: false, err})
+            })
             // user is this document
-            if (err) {
-                console.error;
-                return res.json({
-                    loginSuccess: false,
-                    message: '존재하지 않는 ID'
-                });
-            }
+
             // models/user.js에 작성된 method는 User가 아닌 Document에서 사용되는 객체임
-            user
-                .comparePassword(req.body.pw)
-                .then((isMatch) => {
-                    if(!isMatch) {
-                        return res.json({
-                            loginSuccess: false,
-                            message: '비밀번호가 일치하지 않습니다.'
-                        });
-                    }
-                    user
-                        .generateToken()
-                        .then(user => {
-                            res
-                                .cookie("x_auth", user.token)
-                                .status(200)
-                                .json({
-                                loginSuccess: true,
-                                token: user.token
-                                }); // 꼭 지울것
-                        })
-                        .catch(err => {
-                            res.status(400).send(err);
-                        })
-                })
-                .catch(err => {
-                    res.json({loginSuccess: false, err})
-                })
-        })
+            
+
     });
 }
