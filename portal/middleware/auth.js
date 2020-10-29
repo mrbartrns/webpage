88fbdbad284b8cfa5cd4eed9ee5763ae83331233
Junhyuck <mrbartrns@naver.com>
2020-10-29 19:47:14 +0900

    removed tokenblacklist

--- portal/middleware/auth.js
+++ portal/middleware/auth.js
@@ -1,11 +1,14 @@
 // auth.js는 로그아웃 회원정보 수정 및 여러 요소에 인증을 위한 middleware로 사용이 된다.
 
 const {User} = require('../models/user');
-const {TokenBlackList} = require('../models/token');
+require('dotenv').config();
 
+// const {TokenBlackList} = require('../models/token');
+
+/*
 let auth = (req, res, next) => {
     let token = req.cookies.x_auth;
-
+    
     TokenBlackList
         .isExpired(token)
         .then(isExpired => {
@@ -15,7 +18,7 @@ let auth = (req, res, next) => {
                 User
                     .findByToken(token)
                     .then(user => {
-                        if (!user) return res.json({isAuth: false, error: true});
+                        if (!user) return res.json({isAuth: false, err});
 
                         req.token = token;
                         req.user = {
@@ -28,11 +31,34 @@ let auth = (req, res, next) => {
                         next();
                     })
                     .catch(err => {
-                        throw err;
+                        res.json({isAuth: false, err})
                     })
             }
         })
     
+}
+*/
+let auth = (req, res, next) => {
+    let token = req.cookies.x_auth;
+
+    User
+        .findByToken(token, process.env.ACCESS_SECRET_CODE)
+        .then(user => {
+            if (!user) return res.json({isAuth: false, err});
+
+            req.token = token;
+            req.user = {
+                _id: user._id,
+                id: user.id,
+                nickName: user.nickName,
+                role: user.role
+            };
+
+            next();
+        })
+        .catch(err => {
+            res.json({isAuth: false, message: 'token has been expired.', err})
+        })
 }
 
 module.exports = { auth };
