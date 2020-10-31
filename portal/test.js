120b6ed76fefad378414361bf18a14a6c036c9d4
Junhyuck <mrbartrns@naver.com>
2020-11-01 01:05:28 +0900

    test file

--- portal/test.js
+++ portal/test.js
@@ -1,54 +1,39 @@
-const mongoose = require('mongoose');
-const test = require('./models/test');
+const mongoose = require("mongoose");
+const test = require("./models/test");
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
-mongoose.connect('mongodb://localhost:27017/boards', err => {
-    if (err) console.error;
-    console.log('mongodb server connected');
+mongoose.connect("mongodb://localhost:27017/boards", (err) => {
+  if (err) console.error;
+  console.log("mongodb server connected");
 });
 
-const { Test, Sample } = require('./models/test');
+const { Test, Sample } = require("./models/test");
 
-Test.findOne({testString: 'hi'})
-    .populate({
-        path: '_sample',
-        model: 'samples'
-    })
-    .then(test => console.log(test));
+// for (let i = 0; i < 2; i++) {
+//   const sample = new Sample();
+//   sample.text = "bye";
+//   sample.save();
+//   Test.findOne({ _id: "5f8fe43bfdc88257f8086934" })
+//     .then((test) => {
+//       test._sample.push(sample._id);
+//       return test.save();
+//     })
+//     .then((test) => console.log(test));
+// }
 
-Test.findOne({testString: 'hi'})
-    .then(test => {
-        console.log(test);
-        test.example = 'new new string';
-        test.save()
-            .then(test => console.log(test));
-    })
-/**
- * Post
-        .findById(req.params.postid)
-        .then(post => {
-            console.log('phase 2');
-            likesCount = post.likes;
-            console.log(likesCount);
-            console.log(likeFlag);
-            if (likeFlag) {
-                likesCount++;
-                console.log(likesCount);
-            } else {
-                likesCount--;
-                console.log(likesCount);
-            }
-
-            post.likes = likesCount;
-            console.log('after like:', post);
-            // not working+
-            post.save()
-                .then(post => res.json({success: true, post}))
-        })
-        .catch(err => console.error(err));
-
- */
+console.log(test);
+Test.findOne({ _id: "5f8fe43bfdc88257f8086934" })
+  // can use splice after populate
+  .populate({
+    path: "_sample",
+    model: "samples",
+  })
+  .then((test) => {
+    console.log("type:", typeof test._sample);
+    test._sample.splice(0, test._sample.length);
+    console.log(test);
+  });
