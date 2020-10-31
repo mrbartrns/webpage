63afc467d941c51e8713a33a2ba63dafc558ed0b
Junhyuck <mrbartrns@naver.com>
2020-11-01 01:05:07 +0900

    console.log

--- portal/app.js
+++ portal/app.js
@@ -1,71 +1,74 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const config = require('./config');
-const {User} = require('./models/user');
-const { TokenBlackList } = require('./models/token');
-const { Board } = require('./models/boardname');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const config = require("./config");
+const { User } = require("./models/user");
+const { TokenBlackList } = require("./models/token");
+const { Board } = require("./models/boardname");
 
-const { login } = require('./middleware/islogin');
+const { login } = require("./middleware/islogin");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 // database 연결
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.MONGO_URL, {dbName: process.env.MONGO_DB}, err => {
-  if (err) console.error;
-  console.log('mongodb server connected');
-});
-
-
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect(
+  process.env.MONGO_URL,
+  { dbName: process.env.MONGO_DB },
+  (err) => {
+    if (err) console.error;
+    console.log("mongodb server connected");
+  }
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // set the secret key variable for jwt
-app.set('jwt-secret', config.secret);
+app.set("jwt-secret", config.secret);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(login);
 
 // 로그인 되어있는지 여부를 확인한다.
-app.get('/', login, (req, res) => {
-  console.log('/라우트로 들어왔습니다.');
+app.get("/", (req, res) => {
   let isLogined = req.isLogined;
   console.log(req.user);
   Board.find()
-    .then(boards => {
-      res.render('index', {
-        title: 'portal', 
-        boards: boards, 
+    .then((boards) => {
+      res.render("index", {
+        title: "portal",
+        boards: boards,
         login: isLogined,
-        user: req.user ? req.user : null
+        user: req.user ? req.user : null,
       });
     })
-    .catch(err => {
-      res.render('error');
-    })
+    .catch((err) => {
+      res.render("error");
+    });
 });
 
 // related to login, register routes
-require('./routes/login')(app);
-require('./routes/modify')(app);
-require('./routes/logout')(app, User, TokenBlackList);
-require('./routes/admin')(app, User);
-require('./routes/board')(app, User);
-require('./routes/token')(app);
+require("./routes/login")(app);
+require("./routes/modify")(app);
+require("./routes/logout")(app, User, TokenBlackList);
+require("./routes/admin")(app, User);
+require("./routes/board")(app, User);
+require("./routes/token")(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -76,11 +79,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
