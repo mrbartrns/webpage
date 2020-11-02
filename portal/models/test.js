ccf267703e74a2dbad9d9f0a3b947060db76d5ea
Junhyuck <mrbartrns@naver.com>
2020-11-02 18:55:11 +0900

    test model

--- portal/models/test.js
+++ portal/models/test.js
@@ -18,6 +18,8 @@ const testSchema = new Schema({
       required: true,
     },
   ],
+
+  _newSample: [{ type: Schema.Types.ObjectId, ref: "samples" }],
 });
 
 const sampleSchema = new Schema({
@@ -25,6 +27,24 @@ const sampleSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+// this를 document로 부를때 document: true 옵션 필요 > deleteone의 경우 document, query 둘다 가능 그래서 구분해줘야 함
+
+sampleSchema.pre("deleteOne", { document: true }, function () {
+  console.log("hihihiihhii");
+  let sample = this;
+  console.log("middleware로 들어옴");
+  return sample
+    .model("tests")
+    .updateOne(
+      {
+        _sample: { $in: sample._id },
+      },
+      { $pullAll: { _sample: sample._id } }
+    )
+    .then((res) => res)
+    .catch((err) => err);
 });
 
 const Test = mongoose.model("tests", testSchema);
