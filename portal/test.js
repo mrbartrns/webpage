d3eca3937a72a6d041105264594868069fc4ae75
Junhyuck <mrbartrns@naver.com>
2020-11-05 10:29:41 +0900

    new test

--- portal/test.js
+++ portal/test.js
@@ -3,8 +3,6 @@ const { Sample, Test } = require("./models/test");
 const { User } = require("./models/user");
 const { Post } = require("./models/post");
 const { Comment } = require("./models/comment");
-const { UnorderedCollection } = require("http-errors");
-const e = require("express");
 
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
@@ -16,30 +14,37 @@ mongoose.connect("mongodb://localhost:27017/boards", (err) => {
   console.log("mongodb server connected");
 });
 
-// const test = new Test();
-// test.testString = "test string";
-// test.example = "example string";
+const test = new Test();
+test.testString = "test string";
+test.example = "example string";
+const sample = new Sample();
+sample.text = "test sample";
+sample.save();
+for (let i = 0; i < 2; i++) {
+  test._sample.push(sample);
+}
+test.save();
 
-// for (let i = 0; i < 2; i++) {
-//   const sample = new Sample();
-//   if (i % 2 === 0) {
-//     sample.text = "even sample";
-//     test._sample = sample._id;
-//   } else {
-//     sample.text = "odd sample";
-//     test._newSample = sample._id;
-//   }
-//   sample.save();
-// }
-// test.save();
+// console.log(test);
+// console.log(sample);
 
-// Sample.findOne({ text: "even sample" }).then((sample) => {
-//   sample
-//     .model("samples")
-//     .findOne({ _id: sample._id })
-//     .then((sample) => console.log(sample._id));
-// });
+// Sample.findOne({ text: "test sample" })
+//   .then((sample) => {
+//     sample.deleteOne();
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
 
-Sample.findOne({ text: "even sample" }).then((sample) => {
-  sample.deleteOne();
-});
+/**
+ * findOne => push, save => updateOne이랑 똑같음
+ */
+
+// Test.findOne({ _sample: "5fa351e770afb6276c1e52c7" }).then((test) =>
+//   console.log(test)
+// );
+// Test.findOne({ _id: "5fa351e770afb6276c1e52c6" }).then((test) =>
+//   console.log(test)
+// );
+
+Test.findOne({ _sample: sample }).then((test) => console.log(test));
