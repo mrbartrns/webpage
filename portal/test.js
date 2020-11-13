a88a190ffe93840b4f77aacf638ddb60263ca4c5
Junhyuck <mrbartrns@naver.com>
2020-11-13 16:47:32 +0900

    new test

--- portal/test.js
+++ portal/test.js
@@ -1,50 +1,19 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const { Sample, Test } = require("./models/test");
-const { User } = require("./models/user");
-const { Post } = require("./models/post");
-const { Comment } = require("./models/comment");
+const app = express();
+require("dotenv").config();
 
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
-
-mongoose.connect("mongodb://localhost:27017/boards", (err) => {
-  if (err) console.error;
-  console.log("mongodb server connected");
+app.get("/", (req, res) => {
+  console.log(req.query.name);
+  console.log(req.query.string);
+  res.json({ name: req.query.name, string: req.query.string });
 });
+// view engine setup
 
-const test = new Test();
-test.testString = "test string";
-test.example = "example string";
-const sample = new Sample();
-sample.text = "test sample";
-sample.save();
-for (let i = 0; i < 2; i++) {
-  test._sample.push(sample);
-}
-test.save();
-
-// console.log(test);
-// console.log(sample);
-
-// Sample.findOne({ text: "test sample" })
-//   .then((sample) => {
-//     sample.deleteOne();
-//   })
-//   .then((res) => {
-//     console.log(res);
-//   });
-
-/**
- * findOne => push, save => updateOne이랑 똑같음
- */
-
-// Test.findOne({ _sample: "5fa351e770afb6276c1e52c7" }).then((test) =>
-//   console.log(test)
-// );
-// Test.findOne({ _id: "5fa351e770afb6276c1e52c6" }).then((test) =>
-//   console.log(test)
-// );
-
-Test.findOne({ _sample: sample }).then((test) => console.log(test));
+app.listen(3000, () => {
+  console.log("server listen on 3000");
+});
