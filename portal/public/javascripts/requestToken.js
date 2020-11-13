1ca727c993c54394e9381a08259a0d9a6102da04
Junhyuck <mrbartrns@naver.com>
2020-11-13 18:07:09 +0900

    fixed bug when refresh

--- portal/public/javascripts/requestToken.js
+++ portal/public/javascripts/requestToken.js
@@ -15,8 +15,10 @@
 
   console.log(document.cookie);
   const timer = setTimeout(() => {
-    console.log("로그아웃 실행");
-    fetch("/logout");
+    // console.log("로그아웃 실행");
+    fetch("/logout").then(() => {
+      window.location.href = "/"; //추후에 이전페이지로 이동으로 수정 예정
+    });
   }, 1000 * 60 * 60);
 
   // clearTimeout(timer);
