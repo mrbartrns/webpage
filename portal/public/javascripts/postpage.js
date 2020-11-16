9e1fcc3d8cbaa2a11feb9f01d08165a2f0d93dda
Junhyuck <mrbartrns@naver.com>
2020-11-16 16:19:09 +0900

    comment box wil show at the last of the child

--- portal/public/javascripts/postpage.js
+++ portal/public/javascripts/postpage.js
@@ -29,14 +29,31 @@ window.onload = () => {
   // 답글버튼을 눌렀을 때, comment box가 이동하게 하는 함수
   function showCommentPostBox() {
     let postComment = document.querySelector("#comment-box");
+
     postComment.remove();
+
+    /**
+     * commentRow: the parent comment
+     * childs = comment which have data-parent of parent comment's data-commentsn
+     */
     const commentRow = this.parentNode.parentNode;
+    const childs = document.querySelectorAll(
+      `div[data-parent="${commentRow.dataset.commentsn}"]`
+    );
+    console.log("commentSn:", commentRow.dataset.commentsn);
+    console.log("element:", childs);
+
     const html = `<div class="comment-post-area" id="comment-box">
       <textarea rows="3" oninput="autoGrow(this)" id="comment-write"></textarea>
       <button type="button" id="post-comment" onclick="submitComment(this, '${commentRow.dataset.commentsn}')">글쓰기</button>
     </div>`;
 
-    commentRow.insertAdjacentHTML("afterend", html);
+    if (childs.length !== 0) {
+      childs[childs.length - 1].insertAdjacentHTML("afterend", html);
+    } else {
+      commentRow.insertAdjacentHTML("afterend", html);
+    }
+
     // let postBtn = document.querySelector("#post-comment");
     console.log(commentRow);
   }
