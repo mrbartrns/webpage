63d03f09500dcc4646a7d929ec9a06d13020afd3
Junhyuck <mrbartrns@naver.com>
2020-11-18 18:38:33 +0900

    new test

--- portal/test.js
+++ portal/test.js
@@ -8,6 +8,7 @@ const app = express();
 require("dotenv").config();
 
 const { Test, Sample } = require("./models/test");
+const { Counter } = require("./models/post");
 
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
@@ -25,32 +26,36 @@ mongoose.connect(
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const counter = new Counter();
+counter.order = 0;
+counter.save();
+
 // set the secret key variable for jwt
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.json({ success: true });
-});
+// app.get("/", (req, res) => {
+//   res.json({ success: true });
+// });
 
-app.post("/post", (req, res) => {
-  const test = new Test();
-  test.testString = req.body.string;
+// app.post("/post", (req, res) => {
+//   const test = new Test();
+//   test.testString = req.body.string;
 
-  test
-    .save()
-    .then(() => {
-      res.json({ success: true });
-    })
-    .catch((err) => {
-      res.json({ success: false, err: err });
-    });
-});
+//   test
+//     .save()
+//     .then(() => {
+//       res.json({ success: true });
+//     })
+//     .catch((err) => {
+//       res.json({ success: false, err: err });
+//     });
+// });
 
-app.listen(3000, () => {
-  console.log("server connected");
-});
+// app.listen(3000, () => {
+//   console.log("server connected");
+// });
